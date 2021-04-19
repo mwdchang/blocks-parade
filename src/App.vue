f3c230971e91ded6e07ade22d540654646d269e5
Daniel <mwdchang@gmail.com>
2021-04-19 00:22:24 -0400

    update

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,138 @@
 <template>
-  <BlockHistory />
+  <BlockHistory
+    v-if="blocks.length > 0"
+    :blocks="blocks" />
 </template>
 
 <script>
+import { ref } from 'vue';
 import BlockHistory from './components/BlockHistory.vue';
+import * as data from './assets/cleaned.json';
+import ImageUtil from './libs/image-util';
+
+
+const BASE_W = 230;
+const BASE_H = 120;
+
+const TARGET_W = BASE_W * 2.0;
+const TARGET_H = BASE_H * 2.0;
+
+const TILE_W = 46 * 2;
+const TILE_H = 24 * 2;
+
+
+async function loadThumbnail(blocks, startIdx, endIdx, width, height) {
+  // FIXME use web-workers to speed it up, or preprocess
+  const r = [];
+  for (let i = startIdx; i <= endIdx; i++) {
+    try {
+      const d = await ImageUtil.loadImage(blocks[i].thumbnail, { width, height });
+      blocks[i].imageData = d;
+      r.push(blocks[i]);
+    } catch (err) {
+      // blocks[i].imageData = null;
+    }
+  }
+  return r;
+}
+
+/*
+const findBestImage = (sourceImages, target) => {
+  let r = sourceImages[0];
+  let dist = 999999999;
+  for (let i = 0; i < sourceImages.length; i++) {
+    const d = ImageUtil.distance(target, sourceImages[i].data);
+    if (d < dist) {
+      dist = d;
+      r = sourceImages[i];
+    }
+  }
+  return r;
+};
+*/
+
+const drawRGBA = (ctx, data, w, h, x = 0, y = 0) => {
+  const imageData = ctx.createImageData(w, h);
+  for (let i = 0; i < data.length; i++) {
+    const v = data[i];
+    imageData.data[i] = v;
+  }
+  ctx.putImageData(imageData, x, y);
+};
+
+
 
 export default {
   name: 'App',
   components: {
     BlockHistory
+  },
+  setup() {
+    const blocks = ref([]);
+    return {
+      blocks
+    };
+  },
+  mounted() {
+    console.log('mounted');
+    this.refresh();
+  },
+  methods: {
+    async refresh() {
+      const blocks = data.default;
+      this.blocks = blocks;
+
+      // Load a random target
+      const target = (await loadThumbnail(blocks, 40, 40, 230, 120))[0];
+
+      const canvas = document.createElement('canvas');
+      canvas.width = target.imageData.width;
+      canvas.height = target.imageData.height;
+      canvas.getContext('2d').putImageData(target.imageData, 0, 0);
+
+      // Make the target image 2X larger to better git
+      const canvas2 = document.createElement('canvas');
+      canvas2.id = 'target-image';
+      canvas2.width = TARGET_W;
+      canvas2.height = TARGET_H;
+      const ctx2 = canvas2.getContext('2d');
+      ctx2.drawImage(canvas, 0, 0, TARGET_W, TARGET_H);
+      document.body.append(canvas2);
+
+      const targetImageData2X = ctx2.getImageData(0, 0, TARGET_W, TARGET_H);
+
+      // 1. Load source images
+      // const availableBlocks = await loadThumbnail(blocks, 40, 50, TILE_W, TILE_H);
+      // for (let i = 0; i < availableBlocks.length; i++) {
+      //   const b = availableBlocks[i];
+      //   const canvas = document.createElement('canvas');
+      //   canvas.width = b.imageData.width;
+      //   canvas.height = b.imageData.height;
+      //   canvas.getContext('2d').putImageData(b.imageData, 0, 0);
+      //   document.body.append(canvas);
+      // }
+
+      // 2. Chop and run best matching tile
+      // FIXME: Split into web-worker threads
+      document.body.append(document.createElement('br'));
+
+      for (let i = 0; i < (TARGET_W / TILE_W); i++) {
+        for (let j = 0; j < (TARGET_H / TILE_H); j++) {
+          const tile = ImageUtil.crop(targetImageData2X.data,
+            { width: TARGET_W, height: TARGET_H, channels: 4 },
+            { x: i * TILE_W, y: j * TILE_H, width: TILE_W, height: TILE_H } // reverse W and H
+          );
+
+          const canvas = document.createElement('canvas');
+          canvas.width = TILE_W;
+          canvas.height = TILE_H;
+          drawRGBA(canvas.getContext('2d'), tile, TILE_W, TILE_H);
+          document.body.append(canvas);
+          // canvas.getContext('2d').putImageData(blah, 0, 0);
+        }
+        document.body.append(document.createElement('br'));
+      }
+    }
   }
 };
 </script>
--- src/components/BlockHistory.vue
+++ src/components/BlockHistory.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <svg ref="block"></svg>
+    <svg ref="svg"></svg>
   </div>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
+import { ref } from 'vue';
+import _ from 'lodash';
 import * as d3 from 'd3';
 
+/*
 function test(el) {
   d3.select(el)
     .append('circle')
@@ -16,35 +18,107 @@ function test(el) {
     .attr('r', 20)
     .attr('fill', '#f80');
 }
+*/
 
 export default {
+  props: {
+    blocks: {
+      type: Array
+    }
+  },
   setup() {
-    const block = ref(null);
-    onMounted(() => {
-      test(block.value);
-    });
+    const svg = ref(null);
+    return { svg };
+  },
+  mounted() {
+    this.refresh();
+  },
+  methods: {
+    refresh() {
+      const groupedBlocks = _.groupBy(this.blocks, d => {
+        const t = d.created_at.split('-');
+        return t[0] + '-' + t[1];
+      });
+      this.createTimeline(groupedBlocks);
+    },
+    createTimeline(groupedBlocks) {
+      const keys = Object.keys(groupedBlocks);
+      const values = Object.values(groupedBlocks);
+      console.log(keys);
 
-    return {
-      block
-    };
+      const margin = ({ top: 20, right: 20, bottom: 20, left: 20 });
+      const width = 1000;
+      const height = 100;
+
+      const svg = d3.select(this.svg)
+        .attr('viewBox', [0, 0, width, height]);
+
+      const brush = d3.brushX()
+        .extent([[margin.left, margin.top], [width - margin.right, height - margin.bottom]])
+        .on('start brush end', brushed);
+
+      const x = d3.scaleLinear([0, keys.length - 1], [margin.left, width - margin.right]);
+      const xAxis = g => g
+        .attr('transform', `translate(0, ${height - margin.bottom})`)
+        .call(d3.axisBottom(x).tickValues([0, Math.floor(keys.length / 2), keys.length - 1]).tickFormat(d => {
+          return keys[d];
+        }));
+
+      svg.append('g').call(xAxis);
+
+      // Remove ticks
+      svg.selectAll('.tick').selectAll('line').remove();
+      svg.selectAll('.domain').remove();
+
+      svg.append('g')
+        .attr('fill-opacity', 0.2)
+        .selectAll('circle')
+        .data(values)
+        .join('circle')
+        .attr('transform', (d, i) => {
+          return `translate(${x(i)}, 50)`;
+        })
+        .attr('r', d => {
+          return Math.sqrt(d.length);
+        });
+
+      svg.append('g')
+        .call(brush)
+        .call(brush.move, [0, 5].map(x))
+        .call(g => g.select('.overlay')
+          .datum({ type: 'selection' })
+          .on('mousedown touchstart', beforebrushstarted)
+        );
+
+
+      function brushed(event) {
+        const selection = event.selection;
+        if (selection === null) {
+          // circle.attr('stroke', null);
+        } else {
+          // const [x0, x1] = selection.map(x.invert);
+          // circle.attr('stroke', d => x0 <= d && d <= x1 ? 'red' : null);
+        }
+      }
+
+      function beforebrushstarted(event) {
+        const dx = x(1) - x(0); // Use a fixed width when recentering.
+        const [[cx]] = d3.pointers(event);
+        const [x0, x1] = [cx - dx / 2, cx + dx / 2];
+        const [X0, X1] = x.range();
+        d3.select(this.parentNode)
+          .call(brush.move, x1 > X1 ? [X1 - dx, X1]
+            : x0 < X0 ? [X0, X0 + dx] : [x0, x1]);
+      }
+    }
   }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+svg {
+  /deep/ .tick {
+    opacity: 0.5;
+  }
 }
 </style>
