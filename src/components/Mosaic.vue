af298a5193720b92f0bf723cf326918e98ae4a09
Daniel <mwdchang@gmail.com>
2021-04-19 19:38:32 -0400

    update

---
+++ src/components/Mosaic.vue
@@ -0,0 +1,21 @@
+<template>
+  <div> Mosiac </div>
+</template>
+
+<script>
+export default {
+  props: {
+    blocks: {
+      type: Array
+    }
+  },
+  mounted() {
+    this.refresh();
+  },
+  methods: {
+    async refresh() {
+    }
+  }
+};
+</script>
+
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/blockcollage/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/blocks-parade/' : '/'
 };
